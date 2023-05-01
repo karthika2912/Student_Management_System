3cb5cc246a7a902be44f70dceadd05da312319a8
karthika2912 <pappulakarthika123@gmail.com>
2023-05-01 14:36:04 +0530

    updated

--- student_courses/student_courses/settings.py
+++ student_courses/student_courses/settings.py
@@ -149,7 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'karthika.pappula2001@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
